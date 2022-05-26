3cd24414eff2d944ab8d5f32984f75b3fb9386ad
Gbemisola Odukoya <folaryz@gmail.com>
2022-05-27 00:16:46 +0100

    Creating first input form with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
 function App() {
+  const [todos, setTodos] = useState([]);
+const [input, setInput] = useState("");
+
+const addTodo = (e) => {
+  e.preventDefault(); // this prevents a refresh
+setTodos ([...todos, input]);
+setInput("");
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='App'>
+      <h1>Welcome to my TODO list</h1>
+
+      <form>
+      <input value={input} onChange={e => setInput(e.target.value)} type="text" />
+      <button type='submit' onClick={addTodo}>Add todo</button>
+      </form>
+   
+
+<h2>List of Todos</h2>
+{todos.map(todo => (
+  <p>{todo}</p>
+))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
